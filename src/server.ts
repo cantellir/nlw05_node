c6a3e7c6b6f3ed45c4a359bab9f86dd814f58332
Rodrigo <cantelli.r@gmail.com>
2021-04-21 18:47:50 -0300

    create project

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+    return response.json({
+        message: "Olá NLW05"
+    });
+});
+
+app.post("/", (request, response) => {
+    return response.json({ message: "Usuário salvo com sucesso" })
+});
+
+app.listen(3333, () => console.log("server is running on port 3333"));
