0ae9fb95472ad84f5e81e99cdfdd65929758af06
Rodrigo <cantelli.r@gmail.com>
2021-04-21 21:26:35 -0300

    add SettingsRepository

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,22 @@
 import express from "express";
 
 import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
+app.use(express.json());
+
+app.use(routes);
+
 app.get("/", (request, response) => {
     return response.json({
-        message: "Olá NLW05"
+        message: "Test get - Olá NLW05"
     });
 });
 
 app.post("/", (request, response) => {
-    return response.json({ message: "Usuário salvo com sucesso" })
+    return response.json({ message: "Test post - NLW05" })
 });
 
 app.listen(3333, () => console.log("server is running on port 3333"));
