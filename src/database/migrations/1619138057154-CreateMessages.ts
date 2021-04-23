e284008f9f11eca00eebbd3a73c684ab44076f2c
Rodrigo <cantelli.r@gmail.com>
2021-04-22 21:34:54 -0300

    create user service

---
+++ src/database/migrations/1619138057154-CreateMessages.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateMessages1619138057154 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
